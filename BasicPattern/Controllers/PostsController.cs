448399149ccae3c6a4f8dd3084ec4cc6115b9168
Cheranga Hatangala <cheranga@gmail.com>
2019-02-28 10:05:41 +1100

    Add project files.

---
+++ BasicPattern/Controllers/PostsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BasicPattern.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BasicPattern.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        private const string FakePostsApi = @"https://jsonplaceholder.typicode.com/posts";
+
+        private readonly HttpClient _httpClient;
+
+        public PostsController(IHttpClientFactory httpClientFactory)
+        {
+            if (httpClientFactory == null)
+            {
+                throw new ArgumentNullException(nameof(HttpClient));
+            }
+
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var httpResponse = await _httpClient.GetAsync(FakePostsApi).ConfigureAwait(false);
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, httpResponse.ReasonPhrase);
+            }
+
+            var posts = JsonConvert.DeserializeObject<List<Post>>(await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false));
+            return Ok(posts);
+        }
+    }
+}
